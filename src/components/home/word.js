30804db6b709b07564357cb8424b7ef405dd85c2
sukanyarani <ranisukanya7777@gmail.com>
2023-10-28 21:55:50 +0530

    Validation

--- src/components/home/word.js
+++ src/components/home/word.js
@@ -4,7 +4,7 @@ import './word.css';
 
 
 // Word-> Encrypted 'Where'(one word at a time)
-function Word({word, enterInputValue, setEnterInputValue}) {
+function Word({word, err, enterInputValue, setEnterInputValue}) {
   let regex = /^[a-zA-Z]+$/;
   return (
     <div className="word">
@@ -22,6 +22,7 @@ function Word({word, enterInputValue, setEnterInputValue}) {
             return <div className='inner' >
               {/* Input starts from here */}
               <input maxLength={1}
+              style={{color: err.includes(char)?"red":""}}
             //enterInputValue[char]=> What users has enterned in the box (char shows the encrypted value
             //below the box
             value={enterInputValue[char] || ''}
--- src/pages/play/play.js
+++ src/pages/play/play.js
@@ -1,57 +1,51 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Word from '../../components/home/word';
 import logo from '../../logo.svg';
 import './play.css';
 
 function Play() {
   var puzzle = 'WHERE THERE IS A WILL THERE IS A WAY';
-  const crypt={A:'B', B:'C',C:'D',D:'E',E:'F',F:'G',G:'H',H:'I',I:'J',J:'K',K:'L',L:'M',M:'N',N:'O',O:'P',P:'Q',Q:'R',R:'S',S:'T',T:'U',U:'V',V:'W',W:'X',X:'Y',Y:'Z',Z:'A'}
-  var position={}
-  var encrypted=''
-  var flag=1;
-  const [enterInputValue,setEnterInputValue]=useState({});
+  const crypt = { A: 'B', B: 'C', C: 'D', D: 'E', E: 'F', F: 'G', G: 'H', H: 'I', I: 'J', J: 'K', K: 'L', L: 'M', M: 'N', N: 'O', O: 'P', P: 'Q', Q: 'R', R: 'S', S: 'T', T: 'U', U: 'V', V: 'W', W: 'X', X: 'Y', Y: 'Z', Z: 'A' }
+  const [enterInputValue, setEnterInputValue] = useState({});
+  const [error, setError] = useState([]);
   //Split the puzzle(string) into words
-  puzzle.split('').map((char,index)=>{
-    //Check whether the letter is already present or not
-    if(crypt[char])
-    {
-      encrypted=encrypted+crypt[char]
-      console.log("Inside if")
-    }
-    //enter in else when encounter a special char
-    else{
-      encrypted=encrypted+char
-      console.log("Inside else ", char)
-    }
-    // for mapper -> position : having all the positions of the characters[W, H, E, R]
-    // if(!position[char])
-    // {
-    //   position[char]=[index];
-    // }
-    // else{
-    //   var fetch=position[char];
-    //   fetch.push(index);
-    //   position[char]=fetch;
-    // }
-  })
+  const [encrypted, setEncrypted] = useState('');
+  useEffect(() => {
+    var encrypted ='';
+    var position = {};
+    puzzle.split('').map((char, index) => {
+      //Check whether the letter is already present or not
+      if (crypt[char]) {
+        encrypted = encrypted + crypt[char];
+        position[crypt[char]] = ""
+      }
+      //enter in else when encounter a special char
+      else {
+        encrypted = encrypted + char;
+      }
+    })
+    setEncrypted(encrypted)
+    setEnterInputValue(position)
+  }, [])
   
+
+
   //For checking each letter of the word .. Whether it is correct ? 
   //After clicking on the Let's Check Button
-  const check=()=>{
-    for(const key in enterInputValue){
+  const check = () => {
+    var err = []
+    for (const key in enterInputValue) {
       console.log(`${key}:${enterInputValue[key]}`);
-      if(crypt[enterInputValue[key]]==key)
-      {
+      if (crypt[enterInputValue[key]] == key) {
         //Correct Value; Continue checking
       }
-      else
-      {
+      else {
         console.log('So Sorry! Please try again');
-        flag=0;
+        err.push(key);
       }
     }
-    if(flag==1)
-      console.log('Yahoo !, you did it');
+    console.log(err);
+    setError(err)
   }
   console.log(encrypted)
   return (
@@ -68,11 +62,11 @@ function Play() {
             //passing the splitted word('WHERE') in Word
             //enterInputValue for storing the value entered by user
             //setEnterInputValue for setting new value(encrypted) one 
-           return <div className='quoteInner'><Word word={word} enterInputValue={enterInputValue} setEnterInputValue={setEnterInputValue}/>&nbsp;</div>
-          }) 
+            return <div className='quoteInner'><Word word={word} err={error} enterInputValue={enterInputValue} setEnterInputValue={setEnterInputValue} />&nbsp;</div>
+          })
         }
       </div>
-      <button class ="btn" onClick={check} type="submit">Let's Check</button>
+      <button class="btn" onClick={check} type="submit">Let's Check</button>
     </div>
   );
 }
